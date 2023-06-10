91861dff594c99f0562c3cacb7b86e13711eac4f
DangerousDaniel <danielcox996@gmail.com>
2023-06-09 21:31:22 -0300

    working mobile nav

--- mywebsiterecreationfrontend/components/layout/navbar.jsx
+++ mywebsiterecreationfrontend/components/layout/navbar.jsx
@@ -15,33 +15,37 @@ import { useState } from "react";
 export default function NavBar() {
     const router = useRouter()
 
+    const [mobileMenuIsActive, setMobileMenuIsActive] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setMobileMenuIsActive(!mobileMenuIsActive)
+    }
 
     return (
         <div>
             <nav className="blue-grey darken-3">
                 <div className="nav-wrapper">
-                    <Link href="/"><img src="/images/logo/my_youtube_gaming_logo_2020_v2.2.png" alt="Picture of the author" className="img-nav-mobile img-nav-desktop" /></Link>
+                    <Link href="/"><img src="/images/logo/my_youtube_gaming_logo_2020_v2.2.png" alt="Picture of the author" className="logo-nav-mobile logo-nav-desktop" /></Link>
                     <Link href="/" className="brand-logo">&nbsp;DangerousDan996</Link>
-
+                    <a onClick={toggleMobileMenu} className="sidenav-trigger right waves-effect waves-light btn blue-grey darken-2"><i className="material-icons">menu</i></a>
                     <ul className="right hide-on-med-and-down">
                         <li className={router.asPath == "/" ? "active" : ""}><Link href="/">Home</Link></li>
                         <li className={router.asPath == "/article/category/1" ? "active" : ""}><Link href="/article/category/1">Monstrous Ent Game Demos</Link></li>
                         <li className={router.asPath == "/article/category/2" ? "active" : ""}><Link href="/article/category/2">Other</Link></li>
-                        <li className={router.asPath == "/resume/1" ? "active" : ""}><Link href="/resume/">Resume</Link></li>
+                        <li className={router.asPath == "/resume/" ? "active" : ""}><Link href="/resume/">Resume</Link></li>
                     </ul>
                 </div>
             </nav>
-
-            <div className="center-align blue-grey darken-3 white-text hide-on-large-only">
-                <i className="material-icons">home</i>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <i className="material-icons">movie</i>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <i className="material-icons">view_module</i>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <i className="material-icons">receipt</i>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </div>
+            {mobileMenuIsActive &&
+                <div className="right blue-grey darken-3 nav-sidebar-moblie hide-on-large-only" onClick={toggleMobileMenu}>
+                    <ul>
+                        <li className={router.asPath == "/" ? "nav-item-moblie active-nav-item-moblie" : "nav-item-moblie"} ><Link className="white-text" href="/">Home</Link></li>
+                        <li className={router.asPath == "/article/category/1" ? "nav-item-moblie active-nav-item-moblie" : "nav-item-moblie"}><Link className="white-text" href="/article/category/1">Monstrous Ent Game Demos</Link></li>
+                        <li className={router.asPath == "/article/category/2" ? "nav-item-moblie active-nav-item-moblie" : "nav-item-moblie"}><Link className="white-text" href="/article/category/2">Other</Link></li>
+                        <li className={router.asPath == "/resume/" ? "nav-item-moblie active-nav-item-moblie" : "nav-item-moblie"}><Link className="white-text" href="/resume/">Resume</Link></li>
+                    </ul>
+                </div>
+            }
         </div>
     )
 }
