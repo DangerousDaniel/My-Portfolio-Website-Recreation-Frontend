68aba637edf4f40a64dec599417f9d4efb11455c
DangerousDaniel <danielcox996@gmail.com>
2023-07-05 01:10:15 -0300

    Tab Icon is back!!!

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -19,7 +19,7 @@ export default function App({ Component, pageProps }) {
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/x-icon" href="/my_youTube_gaming_logo_2020_v2.2.png"></link>
+        <link rel="icon" type="image/x-icon" href="images\logo\my_youtube_gaming_logo_2020_v2.2.png"></link>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
         <title> DangerousDan996 | Default Title</title>
